5c425139c095d49a758363d4257d93c590b0e368
Kaizhe Huang <hkz85825915@gmail.com>
2016-03-09 22:14:43 +1100

    Fix issues in deploy script

--- deploy-from-travis.sh
+++ deploy-from-travis.sh
@@ -3,8 +3,9 @@
 echo "Start deplying..."
 
 if [ ! -d ~/.ssh ]; then mkdir ~/.ssh; fi
-openssl aes-156-cbc -k $PASSWORD -in sshkey.enc -out ~/.ssh/id_rsa -d
+openssl aes-256-cbc -k $PASSWORD -in sshkey.enc -out ~/.ssh/id_rsa -d
 chmod 600 ~/.ssh/id_rsa
 
+ssh git@$DEPLOY_SERVER hello
 git remote add vps git@$DEPLOY_SERVER:fiveyellowmice.com.git
-git push vps master
+git push --quiet vps master
