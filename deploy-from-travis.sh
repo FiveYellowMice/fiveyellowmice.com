69d4075e82c4e3f8e38e1ee6c5de4ec8bd12fd2b
Kaizhe Huang <hkz85825915@gmail.com>
2016-03-09 22:23:35 +1100

    Change encryption

--- deploy-from-travis.sh
+++ deploy-from-travis.sh
@@ -6,6 +6,6 @@ if [ ! -d ~/.ssh ]; then mkdir ~/.ssh; fi
 openssl aes-256-cbc -k $PASSWORD -in sshkey.enc -out ~/.ssh/id_rsa -d
 chmod 600 ~/.ssh/id_rsa
 
-ssh git@$DEPLOY_SERVER hello
+ssh -o "StrictHostKeyChecking no" git@$DEPLOY_SERVER hello
 git remote add vps git@$DEPLOY_SERVER:fiveyellowmice.com.git
 git push --quiet vps master
